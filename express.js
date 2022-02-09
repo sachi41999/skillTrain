def12f41fc5f2f445a13ae15cfd82b6d75a641f4
sabyasachi <sachisabya41999@gmail.com>
2022-02-09 19:39:06 +0530

    commit

--- express.js
+++ express.js
@@ -2,13 +2,14 @@ const express=require('express');
 const path=require('path');
 
 const app=express();
+app.use(express.static(path.join(__dirname,'dist/skill-oopz')));
 
-app.use(express.static('./dist/skill-oopz'));
+
 
 app.get('/*',(req,res)=>{
     res.sendFile('index.html',{root:'dist/skill-oopz'})
 });
 
-const port =process.env.PORT||8000;
+const port =process.env.PORT||8002;
 
 app.listen(port);
